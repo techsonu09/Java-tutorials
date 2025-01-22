fa5db4eb7fbed486d6ad83481309f6027c5cdb0d
techsonu09 <thesonuchaudhary07@gmail.com>
2025-01-22 13:52:36 +0530

    new code

---
+++ public class main {.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(string[] args) {
+        //variable
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+        //System.out.println(sum);
+
+        int mul = a * b;
+        System.out.println(mul);
+    }
+}
