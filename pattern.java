4dd975c41646f6bfa044cdd38c564118dcaac7ee
techsonu09 <thesonuchaudhary07@gmail.com>
2025-01-23 16:12:20 +0530

    pattern

---
+++ pattern.java
@@ -0,0 +1,11 @@
+public class pattern {
+ public static void main(string[] args){
+    //output
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+
+   }
+
+}
