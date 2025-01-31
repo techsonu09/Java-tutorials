13b0d53ed8dbda25654cf2e2a086b2348ccc2ecc
techsonu09 <thesonuchaudhary07@gmail.com>
2025-01-31 13:41:53 +0530

    conditions

---
+++ conditions.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class conditions { 
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if(age > 18) {
+            System.out.println("Not Adult");
+        } else {
+            System.out.println("Not Adult");
+        }
+    }
+}
+
+
+
+        
+
+
+    
+    
